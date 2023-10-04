88bc3c0cc6453acbb7140db37aa3c632b31160d6
Kevin Bailey <200544764@student.georgianc.on.ca>
2023-10-04 13:56:24 -0400

    Oct 4 - Data imported to the AWS database, DBUtility class started, basic layout of graph-view done.

---
+++ src/main/java/com/example/advancedjavaassignment1/DBUtility.java
@@ -0,0 +1,15 @@
+package com.example.advancedjavaassignment1;
+
+public class DBUtility {
+
+    private static String dbUser = "Kevin200544764";
+    private static String password = "y2L7gBDmFo";
+
+    /**
+     * jbdc:mysql -> the database driver
+     * 127.0.0.1 -> IP address of the database server
+     * 3306 -> port number of the database server
+     * COMP1011Friday -> name of the database
+     */
+    private static String connectURL = "jdbc:mysql://172.31.22.43:3306/Kevin200544764";
+}
