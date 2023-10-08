994236471a0c5f6c07f91fb49ed96b978e99f52a
Kevin Bailey <200544764@student.georgianc.on.ca>
2023-10-08 15:28:48 -0400

    Oct 08 - test to commit the icon image

--- src/main/java/com/example/advancedjavaassignment1/Main.java
+++ src/main/java/com/example/advancedjavaassignment1/Main.java
@@ -3,6 +3,7 @@ package com.example.advancedjavaassignment1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("graph-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Spotify Streams");
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("images/icon.png")));
         stage.setScene(scene);
         stage.show();
     }
